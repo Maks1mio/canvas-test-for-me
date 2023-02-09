913263b7d9004a3a8eafc210f8fe478f0eb4d014
EvT <44835662+Maks1mio@users.noreply.github.com>
2023-02-09 16:59:17 +0300

    - slide yooo

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const canvas = document.getElementById('canvas');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 const ctx = canvas.getContext('2d');
 const image = document.getElementById('source');
 let x = 0;
-let pos = -1282;
+let pos = 0
 
-image.addEventListener('load', (e) => {
-  for (let index = 0; index < 2500; index++) {
+animate(pos)
+
+function animate(pos) {
+  x = 0;
+  for (let index = 0; index < 5; index++) {
     i = x;
-    ctx.drawImage(image, i + pos, 0, image.width * .05, image.height * .05);
-    x += 255
+    ctx.drawImage(image, i + pos, 0, image.width * .05, image.height * .05); // draw image at current position
+    x += 250
   }
-});
+}
+
+prev.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  pos += 200
+  animate(pos)
+})
+
+next.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  pos -= 200
+  animate(pos)
+})
